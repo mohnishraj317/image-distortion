3b524460a216b0b8e31c98306fa3b4d05697beab
mohnish-raj <mohnishrajmani934@gmail.com>
2024-04-30 17:56:07 +0530

    fixed file issues

--- js/particle.js
+++ js/particle.js
@@ -1,132 +1,125 @@
 class Particle {
-  constructor(x, y, size, color) {
+  constructor(color, x, y, size) {
+    this.color = color;
+    this.baseColor = color;
     this.x = x;
     this.y = y;
-    this.base = {
-      x: x, y: y
-    };
+    this.baseX = x;
+    this.baseY = y;
+    this.base = {x, y};
     this.size = size;
-    this.color = color;
-    
-    this.vx = 0;
-    this.vy = 0;
-    this.maxSpeed = 12;
-    this.mass = innerHeight;
-    this.k = 5;
-    this.b = 1;
-    
-    this.ax = 0;
-    this.ay = 0;
-    
-    Particle.particles.push(this);
-    const that = this;
-    
-    this.vecs = {
-      vel: new Vector(that.vx, that.vy),
-      acc: new Vector(that.ax, that.ay),
-      d: new Vector(0,0),
-      b: new Vector(0,0),
-      m: new Vector(0,0)
-    }
+    this.system = null;
+    this.maxSpeed = 1;
+    this.speed = 0;
+    this.thresRadius = 20;
+    this.randomSeed = Math.random() * 2;
   }
   
   draw(ctx) {
     ctx.save();
+    ctx.beginPath();
     ctx.fillStyle = this.color;
-    ctx.fillRect(this.x-this.size/2, this.y-this.size/2, this.size, this.size);
+    ctx.fillRect(this.x, this.y, this.size, this.size);
     ctx.restore();
   }
   
-  update(ctx) {
-    const { cos, sin, atan, atan2 } = Math;
-    const that = this;
-    
-    const speed = this.maxSpeed;
-    const curSpeed = Math.hypot(this.vx, this.vy);
-    
-    let md = Math.hypot(
-      globalMouse.x - this.x,
-      globalMouse.y - this.y
-    );
-    
-    md = innerHeight*10/md;
-    
-    const mtheta = Math.PI + Math.atan2(
-      globalMouse.y - this.y,
-      globalMouse.x - this.x,
-    );
-    
-    let bd = Math.hypot(
-      this.base.x - this.x,
-      this.base.y - this.y
-    );
-    bd = this.k*bd;
-    
-    const btheta = Math.atan2(
-      this.base.y - this.y,
-      this.base.x - this.x,
-    );
-    
-    const d = this.b*curSpeed;
-    const dtheta = Math.PI + atan2(this.vy, this.vx);//Math.PI + btheta;
-    
-    const a = [
-      [bd, btheta],
-      [d, dtheta],
-      [md, mtheta]
-    ];
-    
-    this.ax = ~~a.map(arg => arg[0]*cos(arg[1])).reduce((a, b) => a + b, 0) / this.mass;
-    this.ay = ~~a.map(arg => arg[0]*sin(arg[1])).reduce((a, b) => a + b, 0) / this.mass;
-    
-    const theta = Math.atan2(this.ay, this.ax);
-    
-    // if (curSpeed > speed) {
-    //   this.vx = speed*cos(theta);
-    //   this.vy = speed*sin(theta);
-    // } else {
-    //   this.vx += this.ax;
-    //   this.vy += this.ay;
-    // }
-    
-    this.vx += this.ax;
-    this.vy += this.ay;
-    
-    this.x += this.vx;
-    this.y += this.vy;
-    
-    Object.entries(this.vecs).forEach(([name, vec]) => {
-      switch (name) {
-        case "vel" :
-          vec.x = that.vx * 10;
-          vec.y = that.vy * 10;
-          break;
-        
-        case "acc" :
-          vec.x = that.ax * that.mass;
-          vec.y = that.ay * that.mass;
-          break;
-        
-        case "d" :
-          vec.x = d*cos(dtheta);
-          vec.y = d*sin(dtheta);
-          break;
-          
-        case "b":
-          vec.x = bd * cos(btheta);
-          vec.y = bd * sin(btheta);
-          break;
-        
-        case "m":
-          vec.x = md*cos(mtheta);
-          vec.y = md*sin(mtheta);
-          break;
-      }
-    });
-    
+  update(ctx, i) {
     this.draw(ctx);
+    
+    const md = Math.hypot(
+      this.system.mouse.x - this.x,
+      this.system.mouse.y - this.y
+    );
+    
+    const theta = Math.PI - Math.atan2(
+      (this.y - this.system.mouse.y),
+      (this.system.mouse.x - this.x),
+    );
+    
+    const speed = (this.randomSeed / Math.log10(md)) + .5;
+    const vx = speed * Math.cos(theta);
+    const vy = speed * Math.sin(theta);
+    
+    this.x += vx;
+    this.y += vy;
   }
-  
-  static particles = [];
+
+  static create = (...args) => new Particle(...args);
 }
 
+class ParticleSystem {
+  constructor(cnv, mouse) {
+    this.cnv = cnv;
+    this.ctx = cnv.getContext("2d") 
+    this.particles = [];
+    this.particleSize = ~~(this.cnv.width / 60);
+    this.mouse = mouse;
+  }
+  
+  addParticle(p) {
+    p.system = this;
+    this.particles.push(p);
+  }
+  
+  createParticlesFromImg(img) {
+    this.ctx.save();
+    this.ctx.drawImage(img, 0, 0, cnv.width, cnv.height);
+    const { data } = this.ctx.getImageData(0, 0, cnv.width, cnv.height);
+    this.ctx.clearRect(0, 0, cnv.width, cnv.height);
+    const size = this.particleSize;
+    
+    for (let y = 0; y < this.cnv.height; y += size)
+      for (let x = 0; x < this.cnv.width; x += size) {
+        const i = ~~(4 * (x + y * cnv.width));
+        
+        const r = data[i];
+        const g = data[i + 1];
+        const b = data[i + 2];
+        const a = data[i + 3] / 255;
+        
+        if (a > 0)
+          this.addParticle(Particle.create(
+            `rgba(${r},${g},${b},${a})`,
+            ~~x, ~~y,
+            size
+          ));
+      }
+    
+    // const view = new Uint32Array(data.buffer)
+    
+    // for (let i = 0; i < view.length; i += size) {
+    //   const x = (i+1) % this.cnv.width;
+    //   const y = i / this.cnv.width + 1;
+      
+    //   const a = (view[i]>>>(8*3)) / 255;
+    //   const b = (view[i]<<8)>>>(8*3);
+    //   const g = (view[i]<<(8*2))>>>(8*3);
+    //   const r = ((view[i]<<(8*3))>>>(8*3));
+      
+    //   if (a > 0)
+    //   this.addParticle(Particle.create(
+    //     `rgb(${r},${g},${b})`,
+    //     ~~x, ~~y,
+    //     10
+    //   ));
+    // }
+    
+    this.ctx.restore();
+  }
+  
+  selectParticles(mouse, radius) {
+    const selected = [];
+    const i = ~~((mouse.x + mouse.y * this.cnv.width / this.particleSize) / this.particleSize);
+    selected.push(this.particles[i + 1]);
+    return selected;
+  }
+  
+  animate(timestamp) {
+    const that = this;
+    requestAnimationFrame(that.animate.bind(that));
+    fillCtx(this.cnv, "#0002");
+    this.particles.forEach((particle, i) => {
+      particle.update(this.ctx, i);
+    });
+  }
+}
